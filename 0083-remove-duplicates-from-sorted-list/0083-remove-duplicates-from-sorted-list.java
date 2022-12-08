59723bf855a36641a8bc7749c8056c5ec9ad52b1
Nitintembhare29 <115885821+Nitintembhare29@users.noreply.github.com>
2022-12-08 15:48:04 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (81.78%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
@@ -10,18 +10,20 @@
  */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
+        // base case 
         if(head==null){
             return null;
         }
-            ListNode curr = head;
-            while(curr.next!=null){
-                if(curr.val==curr.next.val){
-                    curr.next=curr.next.next;
-                }
-                else{
-                    curr=curr.next;
-                }
+        ListNode curr = head;
+        while(curr.next!=null){
+            if(curr.val==curr.next.val){
+                curr.next = curr.next.next;
             }
+            else{
+                curr = curr.next;
+            }
+        }
         return head;
+        
     }
 }
